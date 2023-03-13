59fa8e574329091c71bedc2b88143c7e2c5f31f0
SaiKarthikC-137 <kosurisaikarthik2132@gmail.com>
2023-03-13 14:49:21 +0530

    generation type auto to identity

--- BackEnd/src/main/java/com/example/Validation/models/User.java
+++ BackEnd/src/main/java/com/example/Validation/models/User.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 @Table(name="UserTable")
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer userid;
 	private String username;
 	private String mobile;
